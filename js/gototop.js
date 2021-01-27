470489e0fe3312fe9bf55d2c3128c148a1bbb2d1
IvanBulldog <78094984+IvanBulldog@users.noreply.github.com>
2021-01-27 17:37:56 +0200

    Update gototop.js

--- js/gototop.js
+++ js/gototop.js
@@ -1,18 +1,13 @@
-window.onscroll = function() {scrollFunction()};
+const scrollFunctionBtn = document.querySelector(".back-to-top"); // for taked place top button
 
-function scrollFunction() {
-    const mybutton = document.getElementById("myBtn"); // for taked place top button
-    const navBarScrolling = document.querySelector('.navbar'); // for taked place navbar
-  if (document.documentElement.scrollTop > 20) { // when button and nav must worked
-    mybutton.style.display = "block"; // create button in scroll
-    navBarScrolling.classList.add('top-nav-collapse'); // for add class to navbar
-  } else {
-    mybutton.style.display = "none"; // delete top button in top place
-    navBarScrolling.classList.remove('top-nav-collapse'); // delet class if him in top
-  }
-}
+window.addEventListener('scroll', function scrollFunctionTop() {
+    if (document.documentElement.scrollTop > 20) { // when button and nav must worked
+        scrollFunctionBtn.style.display = "block"; // create button in scroll
+    } else {
+        scrollFunctionBtn.style.display = "none"; // delete top button in top place
+    }
+});
 
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For all another  
-}
+scrollFunctionBtn.addEventListener('click', function fuctionTop() {
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
